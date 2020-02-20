14b817e9609b4aab990052531d62845209d9fc02
skraus5628 <s_read@aol.com>
2020-02-19 21:21:30 -0600

    Update shouts-router.js

--- shouts/shouts-router.js
+++ shouts/shouts-router.js
@@ -3,22 +3,41 @@ const shoutsModel = require("./shouts-model")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-	shoutsModel.find()
-		.then((data) => res.status(200).json(data))
-		.catch((err) => next(err))
+// router.get("/", async (req, res, next) => {
+// 	shoutsModel.find()
+// 		.then((data) => res.status(200).json(data))
+// 		.catch((err) => next(err))
+// })
+
+router.get("/", async (req, res, next) => { 
+	try{
+	const data = await shoutsModel.find()
+	// const somethingElse = await someOtherPromise()
+		 res.status(200).json(data)
+	} catch(err) {
+		next(err)
+	}
 })
 
-router.get("/:id", (req, res, next) => {
-	shoutsModel.findById(req.params.id)
-		.then((data) => res.status(200).json(data))
-		.catch((err) => next(err))
+router.get("/:id", async (req, res, next) => {
+	try{
+	const data = await shoutsModel.findById(req.params.id)
+	res.status(200).json(data)
+	} catch(err){
+		next(err)
+	
+	}
+		
 })
 
-router.post("/", (req, res, next) => {
-	shoutsModel.add(req.body)
-		.then((data) => res.status(201).json(data))
-		.catch((err) => next(err))
+router.post("/", async (req, res, next) => {
+	try{
+		const data= await shoutsModel.add(req.body)
+		 res.status(201).json(data)
+	} catch(err){
+		next(err)
+	}
+		
 })
 
 module.exports = router
